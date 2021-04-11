bf19561359d77cedafc7afbc0edc6b40be5528d1
arclegrandroi <62679207+arclegrandroi@users.noreply.github.com>
2021-04-11 22:55:04 +0200

    x

--- config.sample.ts
+++ config.sample.ts
@@ -2,6 +2,22 @@ import { BotSetting } from "./src/setting"
 
 const config: BotSetting = {
     //bot token
-    token: ""
+    token: "",
+    
+    status: {
+        //type of status 
+        status: 'offline',
+        //if game a enable
+        enable: true,
+        //interval of game update in seconds
+        updateInterval: 30,
+        // games
+        activitys: [
+            {
+                content: 'github',
+                type: 'PLAYING'
+            }
+        ]
+    }
 }
 export default config;
--- src/setting.ts
+++ src/setting.ts
@@ -1,3 +1,25 @@
+import { Status } from "eris";
+
 export interface BotSetting {
     token: string;
+    status: botStatusSetting;
+}
+export interface botStatusSetting {
+    status: Status;
+    enable: boolean;
+    updateInterval: number|null;
+    activitys: BotActivity[];
+}
+export interface BotActivity {
+    content: string;
+    type: ActivityType;
+    url?: string;
+}
+export type ActivityType = keyof typeof ActivityTypes;
+export enum ActivityTypes {
+    PLAYING = 0,
+    STREAMING = 1,
+    LISTENING = 2, 
+    WATCHING = 3,
+    COMPETING = 5
 }
