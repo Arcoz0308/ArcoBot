93220770d8cd6883823ba854f64dfeb3f2e04650
arclegrandroi <62679207+arclegrandroi@users.noreply.github.com>
2021-04-25 15:51:56 +0200

    update

--- src/services/SlashCommands.ts
+++ src/services/SlashCommands.ts
@@ -1,83 +1,43 @@
-import { APIApplicationCommand, APIApplicationCommandOption, APIInteraction, APIInteractionResponse } from "discord-api-types";
+import { APIApplicationCommandOption, APIInteraction} from "discord-api-types";
 import { ArcoClient } from "../Client";
 import { SlashCommand } from "../SlashCommand";
 import { Service } from "./Service";
-import {RequestMethod, MessageFile} from "eris"
+import { SlashCommandApi } from "../api";
 export interface BaseCommand {
     name: string;
     description: string;
     options?: APIApplicationCommandOption[];
     default_permission?: boolean;
 }
-export class SlashCommandService extends Service {
+export class SlashCommandService extends Service{
     public commands: SlashCommand[] = [];
-    private request: (method: RequestMethod, url: string, auth?: boolean, body?: any, file?: MessageFile, _route?: string, short?: boolean) => Promise<Record<string, unknown>|any>;
-    private auth: string;
+    /**
+     * discord slash cmd objects
+     */
+    public rcmd: object[] = [];
+    public api: SlashCommandApi;
     constructor(client: ArcoClient) {
         super(client);
-        this.request = this.client.requestHandler.request;
-        this.auth = this.client.setting.applicatinId;
+        this.api = new SlashCommandApi(client);
     }
-    async init() {
 
+    async init(): Promise<void> {
+        this.initCommands();
     }
-    onClientReady() {
+
+    onClientReady(): void {
         this.client.on('rawWS',event => {
             if(event.t === 'INTERACTION_CREATE') this.onIneraction(event.d as APIInteraction);
         });
     }
-    onIneraction(i: APIInteraction) {
+    async registerCommands(): Promise<void> {
+        
+    }
+    async onIneraction(i: APIInteraction): Promise<void> {
          
     }
-    async initCommands() {
-
-    }
-
-    // global commands methods
-
-    async getCommands(): Promise<APIApplicationCommand[]> {
-        return this.request('GET', `/applications/${this.auth}/commands`, true);
-    }
-    async createCommand(cmd: BaseCommand): Promise<APIApplicationCommand> {
-        return this.request('POST', `/applications/${this.auth}/commands`, true, cmd);
-    }
-    async getCommand(id: string): Promise<APIApplicationCommand> {
-        return this.request('GET', `/applications/${this.auth}/commands/${id}`, true);
-    }
-    async editCommand(id: string, cmd: BaseCommand): Promise<APIApplicationCommand> {
-        return this.request('PATCH', `/applications/${this.auth}/commands/${id}`, true, cmd);
-    }
-    async deleteCommand(id: string): Promise<any> {
-        return this.request('DELETE', `/applications/${this.auth}/commands/${id}`, true);
-    }
-    async bulkOverwriteCommands(cmds: BaseCommand): Promise<APIApplicationCommand[]> {
-        return this.request('PUT', `/applications/${this.auth}/commands`, true, cmds);
-    }
-
-    // guild commands methods
-
-    async getGuildCommands(guildid: string): Promise<APIApplicationCommand[]> {
-        return this.request('GET', `/applications/${this.auth}/guilds/${guildid}/commands`, true);
-    }
-    async createGuildCommand(guildid: string, cmd: BaseCommand): Promise<APIApplicationCommand> {
-        return this.request('POST', `/applications/${this.auth}/guilds/${guildid}/commands`, true, cmd);
-    }
-    async getGuildCommand(guildid: string, id: string): Promise<APIApplicationCommand> {
-        return this.request('GET', `/applications/${this.auth}/guilds/${guildid}/commands/${id}`, true);
-    }
-    async editGuildCommand(guildid: string, id: string, cmd: BaseCommand): Promise<APIApplicationCommand> {
-        return this.request('PATCH', `applications/${this.auth}/guilds/${guildid}/commands/${id}`, true, cmd);
-    }
-    async deleteGuildCommand(guilid: string, id: string): Promise<any> {
-        return this.request('DELETE', `/applications/${this.auth}/guilds/{guild.id}/commands/${id}`, true);
-    }
-    async bulkOverwriteGuildCommand(guildid: string, cmds: BaseCommand): Promise<APIApplicationCommand[]> {
-        return this.request('PUT', `/applications/${this.auth}/guilds/${guildid}/commands`, true, cmds);
-    }
-
-    // interaction
-
-    async createInteractionResponse(reponse: APIInteractionResponse) {
+    
+    async initCommands(): Promise<void> {
 
     }
 }
